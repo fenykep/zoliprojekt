6f2767ee8710a1fc7b0788491178b3c31968d4f5
fenykep <abel.fenykep@gmail.com>
2019-12-21 17:35:14 +0100

    szépül

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="/zoliprojekt/style.css">
+	</head>
+	
+	<body>
+		<div class="register-wrapper">
+			<div class="card">
+				<form action="/zoliprojekt/szoba.php">
+					<h1>Hello</h1>
+					<input type="emai" placeholder="Enter your email">
+					<input type="submit" value="Boom">
+					<!--Itt csekkold, hogy van-e ilyen email a databaseban-->
+				</form>
+				
+
+			</div>
+		</div>
+		<!--<footer>2019 Ábel Bódis C</footer>-->
+	</body>
+</html>
---
+++ szoba.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="/zoliprojekt/style.css">
+	</head>
+	
+	<body>
+		<div class="register-wrapper">
+			<div class="card">
+				<h1 class="roomuid">#Room UID</h1>
+				<h2 class="member">Tóth Zoltán</h2>
+				<h2 title="The '$' sign marks who paid last time " class="member">Bódis Gergely</h2>
+				<h2><a href="/zoliprojekt/index.php">ADD</a></h2>
+			</div>
+		</div>
+		<!--<footer>2019 Ábel Bódis C</footer>-->
+	</body>
+</html>
