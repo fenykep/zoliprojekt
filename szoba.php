eee627845d86ff3ac489018cc1e04b7ce24e3249
fenykep <abel.fenykep@gmail.com>
2019-12-23 23:59:47 +0100

    I'm trying to make the buttons do their job

--- szoba.php
+++ szoba.php
@@ -12,10 +12,23 @@
 			$dbname = "zoliprojekt";
 
 			$conn = mysqli_connect($servername, $username, $password, $dbname);
-
 			// Check connection
 			if (!$conn) {die("Connection failed: " . mysqli_connect_error());}
 			else{echo("");}
+
+			
+ 			
+			if (isset($_POST["gomb"])) {
+
+			$sql = "UPDATE `rooms` SET `turn` = '"."' WHERE `rooms`.`ID` = '".intval($_POST["roomNum"])."'";
+			
+ 			$result = mysqli_query($conn, $sql);
+				if ($conn->query($sql) === TRUE) {
+    				echo "New record created successfully";
+				} else {
+    				echo "Error: " . $sql . "<br>" . $conn->error;
+				}
+			}
 
 			$sql = "SELECT members, turn FROM `rooms` WHERE ID = '".intval($_POST["roomNum"])."'";	
  			$result = mysqli_query($conn, $sql);
@@ -23,13 +36,16 @@
 
  			echo '
  			<div class="card">
+ 				<form method="post" action="/zoliprojekt/szoba.php">
 				<h1 class="roomuid">'.$_POST["roomNum"].'</h1>
  			';
  			for ($x = 0; $x <= sizeof($members)-1; $x++){
- 				if ($x+1==$turn) {echo '<button name="gomb" value="'.strval($x+1).'">'.$members[$x]." $$$</button>";}
- 				else{echo '<button name="gomb" value="'.strval($x+1).'">'.$members[$x]."</button>";}				
+ 				if ($x+1==$turn) {
+ 					echo '<button type="submit" name="gomb" value="'.strval($x+1).'">'.$members[$x]." $$$</button>";}
+ 				else{
+ 					echo '<button type="submit" name="gomb" value="'.strval($x+1).'">'.$members[$x]."</button>";}				
 			}
-			echo "</div>";
+			echo "</form></div>";
 		?>
 		
 		<!--<footer>2019 Ábel Bódis C</footer>-->
